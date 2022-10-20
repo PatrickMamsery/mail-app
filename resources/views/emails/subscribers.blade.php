3cd519597a77a48fdbe1d63b77a97d97848af189
PatrickMamsery <pkrobert1612@gmail.com>
2022-10-20 10:52:24 +0300

    fine

---
+++ resources/views/emails/subscribers.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+# Welcome to the first Newletter
+
+Dear {{$email}},
+We look forward to communicating more with you. For more information visit our blog.
+
+@component('mail::button', ['url' => 'https://saftz.org'])
+Blog
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
